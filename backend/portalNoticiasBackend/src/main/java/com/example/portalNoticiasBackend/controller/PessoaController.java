55956c0e585329228766d4188e79fc9626a46b1a
PedroHMJA <pedromayer.alves@outlook.com>
2025-05-21 21:10:04 -0300

    Implementando o controller Pessoa

--- backend/portalNoticiasBackend/src/main/java/com/example/portalNoticiasBackend/controller/PessoaController.java
+++ backend/portalNoticiasBackend/src/main/java/com/example/portalNoticiasBackend/controller/PessoaController.java
@@ -1,7 +1,18 @@
 package com.example.portalNoticiasBackend.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.portalNoticiasBackend.modelo.Pessoa;
+import com.example.portalNoticiasBackend.repository.PessoaRepository;
+
+
 
 
 
@@ -9,5 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/pessoa")
 public class PessoaController {
+    @Autowired
+    PessoaRepository pessoaRepository;
 
+    @GetMapping("/buscarPessoa/{id}")
+    public ResponseEntity<Pessoa> buscarPessoa(@PathVariable Long id) {
+        Optional <Pessoa> pessoa = pessoaRepository.findById(id);
+        return pessoa.map(ResponseEntity::ok).orElseGet(() ->ResponseEntity.notFound().build());
+    }
+    
 }
--- backend/portalNoticiasBackend/src/main/java/com/example/portalNoticiasBackend/repository/PessoaRepository.java
+++ backend/portalNoticiasBackend/src/main/java/com/example/portalNoticiasBackend/repository/PessoaRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.portalNoticiasBackend.modelo.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
 }
